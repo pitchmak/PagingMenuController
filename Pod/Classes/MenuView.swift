419d0f0d0bd869e7b9fcbc65eae3efa2edca99a1
luxa-ikeda <naoya.ikeda@luxa.co.jp>
2017-12-07 18:39:36 +0900

    Added underline setting to MenuView.

--- Pod/Classes/MenuView.swift
+++ Pod/Classes/MenuView.swift
@@ -91,6 +91,7 @@ open class MenuView: UIScrollView {
         layoutScrollView()
         setupContentView()
         layoutContentView()
+        setupLineView()
         setupRoundRectViewIfNeeded()
         constructor()
         layoutMenuItemViews()
@@ -205,7 +206,18 @@ open class MenuView: UIScrollView {
             contentView.heightAnchor.constraint(equalTo: heightAnchor)
             ])
     }
-
+    
+    fileprivate func setupLineView() {
+        guard case let .draw(height, color, horizontalPadding) = menuOptions.underLine else { return }
+        
+        let border = CALayer()
+        let width = contentView.bounds.width - horizontalPadding * 2
+        border.frame = CGRect(x: horizontalPadding, y: contentView.bounds.height - height, width: width, height: height)
+        border.backgroundColor = color.cgColor
+        
+        contentView.layer.addSublayer(border)
+    }
+    
     fileprivate func constructMenuItemViews(_ menuOptions: MenuViewCustomizable) {
         constructMenuItemViews({
             return MenuItemView(menuOptions: menuOptions, menuItemOptions: menuOptions.itemsOptions[$0], addDiveder: $1)
